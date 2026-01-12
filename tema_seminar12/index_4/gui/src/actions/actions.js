af4ed29db1db255e31c7d45425820774ac1d0362
Copaci Ramona <copaciramona@gmail.com>
2026-01-12 23:51:16 +0200

    Rezolvare tema 12

---
+++ tema_seminar12/index_4/gui/src/actions/actions.js
@@ -0,0 +1,30 @@
+const SERVER = 'http://localhost:8080'
+
+export function getNotes () {
+  return {
+    type: 'GET_NOTES',
+    payload: async () => {
+      const response = await fetch(`${SERVER}/notes`)
+      const data = await response.json()
+      return data
+    }
+  }
+}
+
+export function deleteNote(id){
+
+  return{
+    type:'DELETE_NOTE',
+    payload: async() => {
+      const response = await fetch(`${SERVER}/notes/${id}`,{
+        method: 'DELETE'
+      })
+
+      if(!response.ok){
+        throw new Error('Nu s-a putut sterge notita!');
+      }
+
+      return id;
+    }
+  }
+}
